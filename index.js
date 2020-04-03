510eec5d11315d1194ce69869e2e6f17d815d68b
Rafael Pili <hpcponte@gmail.com>
2020-04-03 01:40:14 -0300

    feature: added classes, email and phone parsing methods for tags computing

--- index.js
+++ index.js
@@ -1,122 +1,107 @@
-const fs = require("fs");
-var parse = require("csv-parse");
-var _ = require("lodash");
+// ********************** CSV PARSER ****************************
+// ******************** MADE BY RAFAEL CABRAL PILI **************
 
-var csvPath = "input.csv";
+const fs = require("fs")
+var parse = require("csv-parse")
+var _ = require("lodash")
+
+var csvPath = "input.csv"
 
 fs.readFile(csvPath, function(err, fileData) {
   parse(fileData, { columns: false, trim: true }, function(err, rows) {
-    const headers = rows[0];
-
-    let emailRowsIndex = rows[0]
-      .map((row, index) => {
-        return row.includes("email") ? [row, index] : null;
-      })
-      .filter(val => val);
-
-    let phoneRowsIndex = rows[0]
-      .map((row, index) => {
-        return row.includes("phone") ? [row, index] : null;
-      })
-      .filter(val => val);
-
-    let eidIdx = rows[0]
-      .map((row, index) => {
-        return row.includes("eid") ? [row, index] : null;
-      })
-      .filter(val => val);
-
-    let fullnameIdx = rows[0]
-      .map((row, index) => {
-        return row.includes("fullname") ? [row, index] : null;
-      })
-      .filter(val => val);
-
-    let invisibleIdx = rows[0]
-      .map((row, index) => {
-        return row.includes("invisible") ? [row, index] : null;
-      })
-      .filter(val => val);
-
-    let seeAllIdx = rows[0]
-      .map((row, index) => {
-        return row.includes("see_all") ? [row, index] : null;
-      })
-      .filter(val => val);
-
-    let classesIndex = rows[0]
-      .map((row, index) => {
-        return row.includes("classes") ? [row, index] : null;
-      })
-      .filter(val => val);
-
-    let eids = [];
+    let eids = []
+    let emailRowsIndex = getRowIndex(rows[0], "email")
+    let phoneRowsIndex = getRowIndex(rows[0], "phone")
+    let eidIdx = getRowIndex(rows[0], "eid")
+    let fullnameIdx = getRowIndex(rows[0], "fullname")
+    let invisibleIdx = getRowIndex(rows[0], "invisible")
+    let seeAllIdx = getRowIndex(rows[0], "see_all")
+    let classesIndex = getRowIndex(rows[0], "class")
 
     let outputIntermediate = rows
       .map(row => {
-        let obj = {};
+        let obj = {}
         if (row[fullnameIdx[0][1]] === "fullname") {
-          return null;
+          return null
         }
 
-        eids.includes(rows[eidIdx[0][1]]) ? null : eids.push(row[eidIdx[0][1]]);
-        obj[fullnameIdx[0][0]] = row[fullnameIdx[0][1]];
-        obj[eidIdx[0][0]] = row[eidIdx[0][1]];
-        obj[seeAllIdx[0][0]] = row[seeAllIdx[0][1]];
-        obj[invisibleIdx[0][0]] = row[invisibleIdx[0][1]];
+        getRowObjectFromFields(
+          eids,
+          rows,
+          eidIdx,
+          row,
+          obj,
+          fullnameIdx,
+          seeAllIdx,
+          invisibleIdx,
+          phoneRowsIndex,
+          emailRowsIndex,
+          classesIndex
+        )
 
-        phoneRowsIndex.forEach(phoneRow => {
-          obj[phoneRow[0]] = row[phoneRow[1]];
-        });
-
-        emailRowsIndex.forEach(emailRow => {
-          obj[emailRow[0]] = row[emailRow[1]];
-        });
-
-        classesIndex.forEach(classesRow => {
-          obj[classesRow[0]] = row[classesRow[1]];
-        });
-
-        return obj;
+        return obj
       })
       .filter(val => val)
 
     let groupedOutput = groupById(outputIntermediate)
-   
-    let addressesPhones = computeAddressesPhone(groupedOutput, eids, phoneRowsIndex)
 
-    let addressesEmails = computeAddressesEmail(groupedOutput, eids, emailRowsIndex) // still have to implement checks
-  });
-});
+    let addressesPhones = computeAddressesPhone(
+      groupedOutput,
+      eids,
+      phoneRowsIndex
+    )
+
+    let addressesEmails = computeAddressesEmail(
+      groupedOutput,
+      eids,
+      emailRowsIndex
+    ) // still have to implement checks
+
+    let classes = computeClasses(groupedRows, eids, classesIndex)
+  })
+})
+
+getRowIndex = function(row, field) {
+  return row
+    .map((row, index) => {
+      return row.includes(field) ? [row, index] : null
+    })
+    .filter(val => val)
+}
 
 /* ***********************************************
  *@param rows - csv parsed data rows
  **************************************************/
 groupById = function(rows) {
   let groupedClasses = _.groupBy(rows, row => {
-    return row["eid"];
-  });
-  return groupedClasses;
-};
+    return row["eid"]
+  })
+  return groupedClasses
+}
 
 /* ***********************************************
  *@param rows - csv parsed data rows
  **************************************************/
 computeAddressesPhone = function(groupedRows, eids, phoneRowsIndex) {
   return eids.map(key => {
-    return phoneRowsIndex.map((phoneRow) => {
-      obj = {}
-      groupedRows[key].forEach(row => {
-        tags = phoneRow[0].split(/,| /).slice(1).filter(val => val)
-        obj['eid'] = key
-        obj['type'] = 'phone'
-        obj['tags'] = tags
-        obj['address'] = row[phoneRow[0]];
-      });
-      if (obj.address) {
-        return obj
-      } else null
-    }).filter(val => val)
+    return phoneRowsIndex
+      .map(phoneRow => {
+        obj = {}
+        groupedRows[key].forEach(row => {
+          tags = phoneRow[0]
+            .split(/,| /)
+            .slice(1)
+            .filter(val => val)
+          obj["eid"] = key
+          obj["type"] = "phone"
+          obj["tags"] = tags
+          obj["address"] = row[phoneRow[0]]
+        })
+        if (obj.address) {
+          return obj
+        } else null
+      })
+      .filter(val => val)
   })
 }
 
@@ -126,18 +111,81 @@ computeAddressesPhone = function(groupedRows, eids, phoneRowsIndex) {
 
 computeAddressesEmail = function(groupedRows, eids, emailRowsIndex) {
   return eids.map(key => {
-    return emailRowsIndex.map((emailRow) => {
-      obj = {}
-      groupedRows[key].forEach(row => {
-        tags = emailRow[0].split(/,| /).slice(1).filter(val => val)
-        obj['eid'] = key
-        obj['type'] = 'email'
-        obj['tags'] = tags
-        obj['address'] = row[emailRow[0]];
-      });
-       if (obj.address) {
-        return obj
-      } else null
-    }).filter(val => val)
+    return emailRowsIndex
+      .map(emailRow => {
+        obj = {}
+        groupedRows[key].forEach(row => {
+          tags = emailRow[0]
+            .split(/,| /)
+            .slice(1)
+            .filter(val => val)
+          obj["eid"] = key
+          obj["type"] = "email"
+          obj["tags"] = tags
+          obj["address"] = row[emailRow[0]]
+        })
+        if (obj.address) {
+          return obj
+        } else null
+      })
+      .filter(val => val)
+  })
+}
+
+/* ***********************************************
+ *@param rows - csv parsed data rows
+ **************************************************/
+
+function computeClasses(groupedRows, eids, classesIndex) {
+  return eids.map(key => {
+    return classesIndex.map(index => {
+      return {
+        eid: key,
+        ..._.reduce(
+          groupedRows[key],
+          (acc, curr) => {
+            if (acc[index[0]]) {
+              acc[index[0]] += curr[index[0]]
+            } else {
+              acc[index[0]] = curr[index[0]]
+            }
+          },
+          {}
+        )
+      }
+    })
+  })
+}
+
+/* ***********************************************
+ *@param rows - csv parsed data rows
+ **************************************************/
+
+function getRowObjectFromFields(
+  eids,
+  rows,
+  eidIdx,
+  row,
+  obj,
+  fullnameIdx,
+  seeAllIdx,
+  invisibleIdx,
+  phoneRowsIndex,
+  emailRowsIndex,
+  classesIndex
+) {
+  eids.includes(rows[eidIdx[0][1]]) ? null : eids.push(row[eidIdx[0][1]])
+  obj[fullnameIdx[0][0]] = row[fullnameIdx[0][1]]
+  obj[eidIdx[0][0]] = row[eidIdx[0][1]]
+  obj[seeAllIdx[0][0]] = row[seeAllIdx[0][1]]
+  obj[invisibleIdx[0][0]] = row[invisibleIdx[0][1]]
+  phoneRowsIndex.forEach(phoneRow => {
+    obj[phoneRow[0]] = row[phoneRow[1]]
+  })
+  emailRowsIndex.forEach(emailRow => {
+    obj[emailRow[0]] = row[emailRow[1]]
+  })
+  classesIndex.forEach(classesRow => {
+    obj[classesRow[0]] = row[classesRow[1]]
   })
 }
